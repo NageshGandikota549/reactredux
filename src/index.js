4a1603fcbbadb7914f35de857ef5c3b7babdf7cc
Nagesh Gandikota <nagesh.gandikota@valuelabs.com>
2021-10-20 16:32:05 +0530

    store path changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reduxStore from "../../reduxapp/src/Redux/store";
+import reduxStore from "./Redux/store";
 import {Provider} from "react-redux";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.min.css";
